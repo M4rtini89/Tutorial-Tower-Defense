7f25f80ce9238d77ed2bf86569f6fc546825c195
MartinVN <m4rtini89@gmail.com>
2018-10-06 12:26:21 +0200

    Place and upgrade monsters

---
+++ Assets/PlaceMonster.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMonster : MonoBehaviour
+{
+    [SerializeField] private GameObject monsterPrefab;
+    private GameObject monster;
+
+    private bool CanPlaceMonster()
+    {
+        return monster == null;
+    }
+
+    private bool CanUpgradeMonster()
+    {
+        if (monster != null)
+        {
+            MonsterData monsterData = monster.GetComponent<MonsterData>();
+            MonsterLevel nextLevel = monsterData.GetNextLevel();
+            if (nextLevel != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void OnMouseUp()
+    {
+        if (CanPlaceMonster())
+        {
+            monster = Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        else if (CanUpgradeMonster())
+        {
+            monster.GetComponent<MonsterData>().IncreaseLevel();
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+            // TODO: Deduct gold
+        }
+    }
+}
